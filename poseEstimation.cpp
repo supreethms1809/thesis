e471126e65e1ba6c56bc9ea7979b1a4d83d99ddd
supreethms1809 <supreethms1809>
2017-08-23 12:42:51 -0600

    added poseEstimation.cpp

---
+++ poseEstimation.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <stdlib.h>
+
+int readValues(std::string text,float *variable,int i);
+
+using std::string;
+using namespace std;
+
+int readValues(std::string text, float *variable, int i)
+{
+ 	float temp;
+	//cout << "value of text " << text << endl;
+	ifstream myReadFile;
+	myReadFile.open(text, ios::in);
+	if (myReadFile.is_open()) 
+	{
+		while (!myReadFile.eof())
+		{
+			myReadFile >> temp;
+			//cout << "value of variable " << variable[i] << endl;
+			variable[i] = temp;
+			i++;                                                                                                                                                                     }
+	}
+  	myReadFile.close();
+	return i;
+}
+		
+
+int main(void)
+{
+	const int row = 2;
+	const int col = 15;
+	const int row1 = 384;
+	const int col1 = 15;
+
+
+	float *xy = new float [row*col];
+	int items = 0;
+
+	items = readValues("exp.txt",xy,items);
+	cout << "value of items :" << items << endl;
+
+	delete[] xy;
+
+}
